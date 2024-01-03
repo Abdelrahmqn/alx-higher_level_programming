d8bd5995b8a192126073255009f373590180ec38
Abdelrahamqn <boda8099@gmail.com>
2024-01-03 21:50:41 +0200

    py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,6 @@ print(f"Last digit of {number:d} is {last_digit:d}", end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
-    print("and is 0", end=" ")
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import string
+alphabets = string.ascii_lowercase
+print(alphabets, end="")
