7c6a26abc7cc47398d19fcbde6e2dbbd4b641d53
Abdelrahamqn <boda8099@gmail.com>
2024-01-03 15:09:42 +0200

    cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * check_cycle - checking if the next pointer cycle
  *
@@ -7,17 +9,17 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t slow = list;
-	listint_t fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	while (fast != NULL && fast->next != NULL)
 	{
+	slow = slow->next;
+	fast = fast->next->next;
 		if (slow == fast)
 		{
 			return (1);
 		}
-		slow = slow->next;
-		fast = fast->next->next;
 	}
 	return (0);
 }
