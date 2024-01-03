cffe0221d63d0c9fe2fc46ff2e7116aeee8aa960
Abdelrahamqn <boda8099@gmail.com>
2024-01-03 14:25:25 +0200

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checking if the next pointer cycle
+ *
+ * @list: the list of cycle.
+ * return: return (0) iif there are no cycle, (1)if there a cycle in it.
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t slow = list;
+	listint_t fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
