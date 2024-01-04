e62bc05a373d288077e3018c7e44148acb1546d1
Abdelrahamqn <boda8099@gmail.com>
2024-01-04 02:42:45 +0200

    c

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-numbers = range(0, 100)
-for i, number in enumerate(numbers):
-    if number < 10:
-        print("0{:d}".format(number), end="")
-    else:
-        print("{:d}".format(number), end="")
-    if i != len(numbers) - 1:
-        print(", ", end="")
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i != 99 else "\n")
