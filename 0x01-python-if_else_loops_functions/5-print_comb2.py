6bb9be9b04f25facd32528528484bc836022655c
Abdelrahamqn <boda8099@gmail.com>
2024-01-04 02:26:29 +0200

    s

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 numbers = range(0, 100)
-for number in numbers:
+for i, number in enumerate(numbers):
     if number < 10:
-        print("0{:d}".format(number), end=" ,")
+        print("0{:d}".format(number), end="")
     else:
-        print("{:d}".format(number), end=" ,")
+        print("{:d}".format(number), end="")
+    if i != len(numbers) - 1:
+        print(", ", end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 9):
     for b in range(a + 1, 10):
-        print(f"{a}{b:0d}", end="")
+        print("{:d}{:0d}".format(a, b), end="")
         if a != 8 or b != 9:
             print(", ", end="")
