5e0011fdbbafd6bf8f0d9c6ed5e51be3c9b8d16d
Abdelrahamqn <boda8099@gmail.com>
2024-01-08 11:57:47 +0200

    add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for row in matrix:
-        _format = ' '.join(map(str, row))
-        print("{}".format(_format))
+    for matrx in matrix:
+        if len(matrx) == 0:
+            print()
+        for i in range(len(matrx)):
+            if i is len(matrx) - 1:
+
+                print(end="\n""{:d}".format(matrix[i]) else " ")
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 2 - len(tuple_a)
+    b = 2 - len(tuple_b)
+    if a < 0:
+        return tuple_a
+    elif b < 0:
+        return tuple_b
+    tuple_a += (0,) * a
+    tuple_b += (0,) * b
+
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        return res
+
+    return (0, 0)
