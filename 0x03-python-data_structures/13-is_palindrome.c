ae3b17a77cf256afa472309fcf237f8498fdaec8
Abdelrahamqn <boda8099@gmail.com>
2024-01-09 00:52:53 +0200

    c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,36 @@
 #include "lists.h"
 /**
- * is_palindrome - function in c checks if a singly linked list is plalindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: Pointer to the head of the list
  *
- * @head: the head of the list
- *
- * Retrun: 0 if it is not a plandirome, 1 if it is.
-*/
+ * Return: 0 if it is not a palindrome, 1 if it is.
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *fast = *head;
-    listint_t *slow = *head;
-    listint_t *prv = NULL;
-    listint_t *temp = *head;
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *prv = NULL;
+	listint_t *temp = *head;
 
-    while (fast && fast->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        temp->next = NULL;
-    }
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		temp->next = NULL;
+	}
 
-    if (fast != NULL)
-    {
-        slow = slow->next;
-    }
-     while (slow != NULL)
-     {
-        if (prv->n != slow->n)
-        {
-            return 0;
-        }
-        prv = prv->next;
-        slow = slow->next;
-    }
-
-    return 1;
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+	while (slow != NULL)
+	{
+	if (prv->n != slow->n)
+	{
+		return (0);
+	}
+	prv = prv->next;
+	slow = slow->next;
+	}
+return (1);
 }
