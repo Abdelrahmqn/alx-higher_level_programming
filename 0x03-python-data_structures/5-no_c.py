dcd5e3db2a409194c7a52fc5a89882b296f6e00b
Abdelrahamqn <boda8099@gmail.com>
2024-01-08 10:53:11 +0200

    jo

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_s = ""
-    for s in my_string:
-        if s != 'c' and s != 'C':
-            new_s += s
-        return new_s
+    for idx in range(len(my_string)):
+        if my_string[idx] != 'c' and my_string[idx] != 'C':
+            new_s += my_string[idx]
+    return new_s
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for row in matrix:
         _format = ' '.join(map(str, row))
         print("{}".format(_format))
