3eca28704d8a25af75f64a565818c04671f0e91d
Abdelrahamqn <boda8099@gmail.com>
2024-01-08 22:25:01 +0200

    rows and elements also nights

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for matrx in matrix:
-        if len(matrx) == 0:
-            print()
-        for i in range(len(matrx)):
-            if i is len(matrx) - 1:
-
-                print(end="\n""{:d}".format(matrix[i]) else " ")
+    for r in range(len(matrix)):
+        for e in range(len(matrix[r])):
+            print("{} ".format(matrix[r][e]), end="")
+        print()
