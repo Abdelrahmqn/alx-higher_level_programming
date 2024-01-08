161209dfb5a2581be1ce5945f5d5e5600dacf940
Abdelrahamqn <boda8099@gmail.com>
2024-01-09 00:40:41 +0200

    push

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * is_palindrome - function in c checks if a singly linked list is plalindrome
+ *
+ * @head: the head of the list
+ *
+ * Retrun: 0 if it is not a plandirome, 1 if it is.
+*/
+int is_palindrome(listint_t **head)
+{
+    listint_t *fast = *head;
+    listint_t *slow = *head;
+    listint_t *prv = NULL;
+    listint_t *temp = *head;
+
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        temp->next = NULL;
+    }
+
+    if (fast != NULL)
+    {
+        slow = slow->next;
+    }
+     while (slow != NULL)
+     {
+        if (prv->n != slow->n)
+        {
+            return 0;
+        }
+        prv = prv->next;
+        slow = slow->next;
+    }
+
+    return 1;
+}
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return None
-    for r in range(len(matrix)):
-        for e in range(len(matrix[r])):
-            print("{} ".format(matrix[r][e]), end="")
         print()
+    else:
+        for rows in range(len(matrix)):
+            for elements in range(len(matrix[rows])):
+                if elements != len(matrix[rows]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[rows][elements]), end=endspace)
+            print()
