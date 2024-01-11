509fc34d770597cec6d8df7efd79718e03a7bef4
Abdelrahamqn <boda8099@gmail.com>
2024-01-11 05:00:01 +0200

    x

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new = [list(map(lambda o: o * number, i)) for i in my_list]
+    new = [list(map(lambda x: x * number, i)) for i in my_list]
     return new
