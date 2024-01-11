6af7f17dc0e69f8ae0b946c335c4b8b9c03808c9
Abdelrahamqn <boda8099@gmail.com>
2024-01-11 23:26:37 +0200

    return (0, sentencename)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    del my_list[idx]
-    return my_list
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
