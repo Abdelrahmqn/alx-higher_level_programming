f8dcd5d5279a0397aad9bc0e10b520b871980480
abdlerahman mohamed <boda8099@gmail.com>
2024-01-14 18:19:16 +0200

    last + 5 +u

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    lstdigit = number % 10
-    if lstdigit > 0:
-        print(lstdigit)
-    else:
-        print(lstdigit)
+        number = abs(number)
+        num = number % 10
+        print("{}".format(num), end='')
+        return num
