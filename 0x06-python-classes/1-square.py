4e0095b200c5bada17c1e001176d4d4220a813c8
abdlerahman mohamed <boda8099@gmail.com>
2024-01-25 18:34:16 +0200

    2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a square."""
+
+
+class Square:
+    """the class square defined."""
+    def __init__(self, size):
+        """size (no type/value verification)"""
+        self.__size = size
