95cf1147792b87037001529536d3bf5bf4ee74f7
abdlerahman mohamed <boda8099@gmail.com>
2024-02-04 00:33:42 +0200

    0-

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-    function that adds two integers.
-    funciton must do :
-    a and b must be first casted to integers if they are float.
+    a: int
+    b: int
+        a + b is int.
 """
 
 
 def add_integer(a, b=98):
+    """
+    function that adds two integers.
+    funciton must do :
+    a and b must be first casted to integers if they are float.
+"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
