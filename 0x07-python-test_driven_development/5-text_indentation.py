4f69cdcfa0ad4a04bc7e0ca09380b2a6d47407b3
abdlerahman mohamed <boda8099@gmail.com>
2024-02-04 23:38:14 +0200

    hit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a function that prints a text
+with 2 new lines after each of these characters:=> '.', '?' and ':' .
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if i == ".":
+            print()
+            print()
+        elif i == "?":
+            print()
+            print()
+        elif i == ":":
+            print()
+            print()
+        else:
+            print(i, end="")
