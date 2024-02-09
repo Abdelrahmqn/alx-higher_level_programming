fa8c173a0cfbb28106a3c04661b3c51809a2c066
abdlerahman mohamed <boda8099@gmail.com>
2024-02-09 19:38:23 +0200

    look

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""read file with open statement and open default as a read
+"""
+
+
 def read_file(filename=""):
+    """function to read the file text
+    """
+
     with open(filename, encoding="utf-8") as myfile:
         myfile.read()
