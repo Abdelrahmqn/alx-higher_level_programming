9d176cfc414008d863732dd337fb7aea1feeee16
abdlerahman mohamed <boda8099@gmail.com>
2024-03-05 22:20:55 +0200

    ""

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,7 @@
 
 import json
 
+
 def from_json_string(my_str):
     """returns the json.
     """
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """from json file do.
     """
