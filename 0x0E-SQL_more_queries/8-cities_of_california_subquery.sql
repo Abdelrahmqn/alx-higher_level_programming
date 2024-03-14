f0841af0653f3fe7fe463054e710fe4f50971f6b
abdlerahman mohamed <boda8099@gmail.com>
2024-03-14 04:34:37 +0200

    try

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- cities of california subquery tell me now
+SELECT id, name FROM cities WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
+-- queries like requesting the information.
