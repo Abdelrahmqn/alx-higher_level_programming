e14ee7b047ff6536a358a391eaf796b222ef9b96
abdlerahman mohamed <boda8099@gmail.com>
2024-03-15 21:43:23 +0200

    try

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
---Import the database dump of hbtn_0d_tvshows to your MySQL server
+-- Import the database dump of hbtn_0d_tvshows to your MySQL server
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows RIGHT JOIN tv_show_genres
+FROM tv_shows LEFT JOIN tv_show_genres
 --    -LEFT-        -RIGHT-    --
 ON
     tv_shows.id = tv_show_genres.show_id
