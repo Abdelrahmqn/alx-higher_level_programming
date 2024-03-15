8764864a7b3107b034f5083cb193f39645c5528d
abdlerahman mohamed <boda8099@gmail.com>
2024-03-15 03:12:50 +0200

    t

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- displays only the highest 3 TEMP
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
+WHERE `month` = 7 or `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
