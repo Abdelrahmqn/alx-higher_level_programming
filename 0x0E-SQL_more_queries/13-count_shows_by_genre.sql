feb8cfb80f34bab6a0b1d0b6f84306f815eca250
abdlerahman mohamed <boda8099@gmail.com>
2024-03-15 23:06:13 +0200

    t

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- select all genres by counting them
-SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
-FROM tv_genres RIGHT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre FROM tv_shows AS shows
+WHERE id = (
+    WHERE id FROM shows
+    genre = 'Dexter'
+)
+ORDER BY genre ASC;
