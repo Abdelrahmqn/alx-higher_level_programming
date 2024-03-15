9bce693040561f2c425802471496fa4160d7268b
abdlerahman mohamed <boda8099@gmail.com>
2024-03-15 21:35:13 +0200

    try

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows JOIN tv_show_genres
 --    -LEFT-        -RIGHT-    --
 ON
-tv_show_genres.genre_id = tv_shows.id
-
+    tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
