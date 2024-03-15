d25e8c6013c1ef6c35d53b08a0ee57d83b442544
abdlerahman mohamed <boda8099@gmail.com>
2024-03-15 02:03:35 +0200

    e

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,7 +8,7 @@ CONVERT TO CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-MODIFY COLUMN name
+CHANGE COLUMN name
 VARCHAR(256)
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
