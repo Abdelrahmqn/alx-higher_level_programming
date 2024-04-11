3dee7f7037c428419bf3775133475c5cbf0ad1a3
Abdelrahamqn <boda8099@gmail.com>
2024-04-11 23:41:14 +0200

    semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (isNaN(process.argv[2]) || process.argv[2] === 1) {
   console.log(0);
 } else {
   const PRocess = process.argv.map(Number)
-    .slice(2, process.argv.length)
+    .slice(2)
     .sort((a, b) => a - b);
   console.log(PRocess[PRocess.length - 2]);
 }
