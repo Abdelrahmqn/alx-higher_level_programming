c86c8809aa50be51bf242a01de3a993505053798
Abdelrahamqn <boda8099@gmail.com>
2024-04-13 02:08:41 +0200

    adv: 103

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,1 @@
-#!/usr/bin/node
-function incrementAndCall (number, theFunction) {
-  const incremented = number + 1;
-  theFunction(incremented);
-}
-module.exports = incrementAndCall;
+function incrementAndCall(number, theFunction) {
