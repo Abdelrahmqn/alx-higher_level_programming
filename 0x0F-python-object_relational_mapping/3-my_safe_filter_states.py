5ab6cf54b9a6a1781a64bae49421ee3e81959dab
Abdelrahamqn <boda8099@gmail.com>
2024-04-16 01:26:41 +0200

    add

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Manipulate Data"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                          db=sys.argv[3], port=3306, host='localhost')
+
+    curse = con.cursor()
+
+    curse.execute("""SELECT id, name FROM states
+                  WHERE name LIKE BINARY %s
+                  ORDER BY states.id ASC""", (sys.argv[4] + '%',))
+
+    rows = curse.fetchone()
+
+    for row in rows:
+        print(row)
+
+    con.close()
