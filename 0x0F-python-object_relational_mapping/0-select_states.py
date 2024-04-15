6f518549d79e29a30f54ca9a585753af315162af
Abdelrahamqn <boda8099@gmail.com>
2024-04-15 23:53:18 +0200

    try

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
-    conn = MySQLdb.connect(user=user, passwd=passwd, db=db, port=3306, host="localhost")
+    conn = MySQLdb.connect(user=user, passwd=passwd, db=db, port=3306,
+                           host="localhost")
 
     cursor = conn.cursor()
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
@@ -22,4 +23,3 @@ if __name__ == "__main__":
         print(row)
 
     conn.close()
-    cursor.close()
