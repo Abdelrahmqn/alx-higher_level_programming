1831bfdff791d6e0083c39e1d82d631611dad77f
Abdelrahamqn <boda8099@gmail.com>
2024-04-17 13:04:53 +0200

    7-model_state_fetch_all.

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(port=3306, host='localhost', user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
+
+    cur.execute("""SELECT id, name FROM states
+                    ORDER BY states.id ASC""")
+
+    Rows = cur.fetchall()
+
+    for row in Rows:
+        print(*row, sep=': ')
+
+    con.close()
