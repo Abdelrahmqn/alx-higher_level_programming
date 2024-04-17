87679f995f2ed53613d3e96c1f2e70d7813edbae
Abdelrahamqn <boda8099@gmail.com>
2024-04-17 17:34:26 +0200

    add

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,26 +3,48 @@
    print the frist"""
 
 import sqlalchemy
+import MySQLdb
 import sys
-from sqlalchemy import MetaData, create_engine
-from model_state import State, Base
-from sqlalchemy.orm import session, sessionmaker
-
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
-arg3 = sys.argv[3]
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("""mysql+mysqldb://{}:
-                           {}@localhost/{}""".format(arg1,
-                                                     arg2, arg3),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    con = MySQLdb.connect(port=3306, host='localhost', user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
 
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        if state.id == 1:
-            print("{}: {}".format(state.id, state.name))
+    cur.execute("""SELECT id, name FROM states
+                   WHERE states.id = 1
+                   ORDER BY states.id ASC""")
 
-    session.close()
+    Rows = cur.fetchall()
+
+    for row in Rows:
+        print(*row, sep=': ')
+
+    con.close()
+
+
+# import sqlalchemy
+# import sys
+# from sqlalchemy import MetaData, create_engine
+# from model_state import State, Base
+# from sqlalchemy.orm import session, sessionmaker
+
+# arg1 = sys.argv[1]
+# arg2 = sys.argv[2]
+# arg3 = sys.argv[3]
+
+# if __name__ == "__main__":
+#     engine = create_engine("""mysql+mysqldb://{}:
+#                            {}@localhost/{}""".format(arg1,
+#                                                      arg2, arg3),
+#                            pool_pre_ping=True)
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     states = session.query(State).order_by(State.id).all()
+#     for state in states:
+#         if state.id == 1:
+#             print("{}: {}".format(state.id, state.name))
+
+#     session.close()
