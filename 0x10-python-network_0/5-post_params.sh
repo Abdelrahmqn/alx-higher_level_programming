0e5a78ab3a8d7b6d2f142473b939e1768fac5d21
Abdelrahamqn <boda8099@gmail.com>
2024-05-04 22:18:11 +0300

    gg

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a Post req, email header with value: test@gmail.com, .....etc
-curl -s -X POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
