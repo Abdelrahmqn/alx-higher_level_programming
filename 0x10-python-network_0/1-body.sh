856aee9bf4f8935cde11811581bb5a80687ef3c2
Abdelrahamqn <boda8099@gmail.com>
2024-05-04 21:23:02 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a GET reQ to the URL, and display body of response.
-curl -s -w "%{http_code}" "$1" | echo -n "Route $(wc -c)"
+curl -s -w "%{http_code}" "$1" | grep -q "200" | echo -n "Route $(wc -c)"
