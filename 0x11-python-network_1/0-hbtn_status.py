6fa3a7b86b94b1827dab68bdae922eebc34e0a59
Abdelrahamqn <boda8099@gmail.com>
2024-05-06 17:44:29 +0300

    g

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Module Documentation"""
 
-from urllib.request import urlopen
+import urllib.request
 
-
-with urlopen("https://alx-intranet.hbtn.io/status") as res:
-    content = res.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    cn = content.decode('utf-8')
-    print("\t- utf8 ontent: {}".format(cn))
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        cn = content.decode('utf-8')
+        print("\t- utf8 ontent: {}".format(cn))
