1c681916dfdead4b0bc6789586ab808e7d612d09
Abdelrahamqn <boda8099@gmail.com>
2024-05-06 20:13:47 +0300

    f.reason

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,5 +16,3 @@ if __name__ == "__main__":
             print(response.read().decode('utf-8'))
     except HTTPError as f:
         print("Error code: {}".format(f.code))
-        print("Error code: {}".format(f.reason))
-        print("Error code: {}".format(f.headers))
