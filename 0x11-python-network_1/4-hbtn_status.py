62bbae8920489c8b17d61512fc2ec201e5095f36
Abdelrahamqn <boda8099@gmail.com>
2024-05-07 02:26:54 +0300

    move

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""request from url"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    if req.status_code == 200:
+        print("\t- content: OK")
