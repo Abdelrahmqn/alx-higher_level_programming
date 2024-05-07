a7605c3738bdeb6a6b61f8d5d90b9a96773bc051
Abdelrahamqn <boda8099@gmail.com>
2024-05-07 03:32:38 +0300

    ss

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,9 +4,9 @@
 if __name__ == "__main__":
     import requests as r
     from sys import argv as a
-    from requests.exceptions import HTTPError
 
-    try:
-        req = r.get(a[1])
-    except HTTPError as f:
-        print("Error code: {}".format(f.response.status_code))
+    req = r.get(a[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
     import requests as r
     import json as j
     from sys import argv as a
-    if len(a[1]):
-        q = a[1]
-    else:
+    if len(a) == 1:
         q = ""
+    else:
+        q = a[1]
 
     dict_ = {'q': q}
     req = r.post('http://0.0.0.0:5000/search_user', data=dict_)
     dct_req = req.j()
     try:
-        if dct_req:
-            print("[{}] {}".format(dct_req['id'], dct_req['name']))
-        else:
+        if dct_req == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(dct_req.get("id"), dct_req.get("name")))
     except ValueError:
         print("Not a valid JSON")
