24f72432f74fe8844301dbeb62de2ff366e6977d
Abdelrahman <boda8099@gmail.com>
2024-05-31 20:01:00 +0200

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+request.get(url, function (error, response, body) {
   if (!error) {
     const chars = JSON.parse(body).chars;
     printCharacters(chars, 0);
@@ -9,7 +9,7 @@ request(url, function (error, response, body) {
 });
 
 function printCharacters (chars, index) {
-  request(chars[index], function (error, response, body) {
+  request.get(chars[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < chars.length) {
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
-const request = require('request');
 
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const rs = JSON.parse(body).rs;
-    console.log(rs.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
-  }
+const request = require('request');
+furl = process.argv[2];
+
+request.get(furl, function (err, response, body) {
+    if (!err) {
+        const results = JSON.parse(body).results;
+        console.log(results.reduce((count, movie) => {
+          return movie.characters.find((character) => character.endsWith('/18/'))
+            ? count + 1
+            : count;
+        }, 0));
+    }
 });
